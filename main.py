d22de4c16aab2a4a59f7c6a25b6f434b82de32ac
yamini <yaminisatyanarayan@gmail.com>
2026-03-30 12:18:39 +0530

    (Dependency Injection + Middleware + CORS

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 # import fastapi clall from mfasapi module
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 from pydantic import BaseModel,Field
 from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
 #create instace of fastapi
 #app object is used to define all api routes
 app =FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  #allow all (for dev)
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Product(BaseModel):
     name:str =Field(...,min_length=3)
@@ -12,11 +20,15 @@ class Product(BaseModel):
     is_available:bool
     description:Optional[str]=None
     
+    
+def get_current_user():
+    return "current user" 
+# Dependency function
 
 #route home
 @app.get("/")
-def home():
-    return {"hey,its my fast api learning"} #basic health check
+def read(user=Depends(get_current_user)):
+    return {"message":user} #basic health check
 
 @app.get("/user/{id}")#path parameter 
 def get_user(id:int):# id directly taken from url (required)
@@ -31,3 +43,10 @@ def search(q:str =None):
 @app.post("/product",response_model=Product)
 def create_product(product:Product): #api data receive data from request body and validate using pydantic model
     return product
+
+@app.middleware("http")
+async def log_requests(request, call_next):
+    print("Request received")
+    response = await call_next(request)
+    print("Response sent")
+    return response
